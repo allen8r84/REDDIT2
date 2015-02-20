7b63e631a5748e21fc878abb7d599483afcedadc
Jon <jon@allen8r.com>
2015-02-19 19:46:55 -0700

    finish step 3 and moving on to step 4

--- controller.js
+++ controller.js
@@ -2,6 +2,11 @@ var app = angular.module('reddit');
 
 app.controller('PostsController', function($scope, FirebaseService) {
     
-  
+  var getPosts = function() {
+      FirebaseService.getPosts().then(function(data) {
+          $scope.posts = data;
+      });
+  }
+  getPosts();
     
 });
--- service.js
+++ service.js
@@ -2,5 +2,13 @@ var app = angular.module('reddit');
 
 app.service('FirebaseService', function($http, $q) {
     
+    this.getPosts = function(){
+        var deferred = $q.defer();
+        $http.get('https://devmtn.firebaseio.com/posts.json').then(function(res) {
+            res = res.data;
+            deferred.resolve(res);
+        });
+        return deferred.promise;
+    }
     
 });
